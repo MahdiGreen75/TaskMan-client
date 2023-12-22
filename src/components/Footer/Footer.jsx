3e29184569bd9bdf6dee6d498bbb98719ef01219
Ricky Ponting <qarimahdi217@gmail.com>
2023-12-22 09:19:14 +0600

    Make the homepage with navbar and footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@
 const Footer = () => {
     return (
         <>
-            Footer
+            <footer className="footer text-white footer-center p-4 font-bold bg-blue-500 border-t-2 border-blue-600">
+                <aside>
+                    <p>Copyright © 2023 - All right reserved by TaskMan corporation.</p>
+                </aside>
+            </footer>
         </>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,16 @@
 
 const Home = () => {
     return (
-        <div>
-            Thisi si home
+        <div className="min-h-screen ">
+            <div className="hero min-h-screen bg-gradient-to-r from-cyan-300 to-blue-300 ">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Hello there</h1>
+                        <p className="py-6">Welcome to this real life task management tool.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
